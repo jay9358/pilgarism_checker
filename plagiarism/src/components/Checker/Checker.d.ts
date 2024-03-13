3685f9bff2809be6457f6002c6831e543c913efb
Jay <124623992+jay9358@users.noreply.github.com>
2024-03-13 13:17:49 +0530

    s

---
+++ plagiarism/src/components/Checker/Checker.d.ts
@@ -0,0 +1,5 @@
+// Checker.d.ts
+declare module './components/Checker/Checker' {
+  const Checker: React.FC<Props>; // Replace Props with the actual props type of your Checker component
+  export default Checker;
+}
