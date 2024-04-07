f6bd26fca9fe311258c50b3b82cbd558d4e81667
Jay <jayaggarwal99288@gmail.com>
2024-04-07 16:59:08 +0530

    yes

--- plagiarism/src/main.tsx
+++ plagiarism/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
 import { DataProvider } from './components/DataContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
